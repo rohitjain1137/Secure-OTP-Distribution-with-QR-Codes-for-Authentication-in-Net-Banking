6dacc7fc2caefee267fe988ea8c79755b5f5fe67
rohitjain1137 <48173366+rohitjain1137@users.noreply.github.com>
2023-01-09 16:26:10 +0000

    Add files via upload

---
+++ Bank_qr/App_Code/_DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for _DBConnection
+/// </summary>
+public class _DBConnection
+{
+
+    public static SqlConnection _objcon = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString);
+	
+}
